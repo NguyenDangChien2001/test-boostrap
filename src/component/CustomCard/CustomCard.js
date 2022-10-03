143183d6e5ab55ff85589347217757bc53f451b4
NguyenDangChien2001 <nguyendangchien10012001@gmail.com>
2022-10-03 13:36:44 +0700

    test4

--- src/component/CustomCard/CustomCard.js
+++ src/component/CustomCard/CustomCard.js
@@ -1,45 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { videos } from "../../asset";
 
 function CustomCard(props) {
-  const [hide, setHide] = useState("none");
-  const [scale, setScale] = useState(1);
+  const [hide, setHide] = useState(true);
 
   const mouseOver = () => {
-    setHide("block");
-    setScale(1.5);
+    setHide(false);
   };
 
   const mouseLeave = () => {
-    setHide("none");
-    setScale(1);
+    setHide(true);
   };
 
   return (
     <div onMouseOver={() => mouseOver()} onMouseLeave={() => mouseLeave()}>
       <Card
-        style={
-          ({ border: "dark" },
-          { padding: "0 0.05rem" },
-          { transform: `scale(${scale})` })
-        }
-        className="card border-dark bg-warning text-light"
+        style={({ border: "dark" }, { padding: "0 0.05rem" })}
+        className="card border-dark bg-dark text-light"
       >
-        <Card.Img
-          variant="top"
-          src={props.img}
-          role="button"
-          style={{ display: hide }}
-        />
-        <video autoPlay muted className="w-100">
-          <source src={videos.AthenaTrailer} type="video/mp4" />
-        </video>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        {hide && <Card.Img variant="top" src={props.img} role="button" />}
+        {!hide && (
+          <video autoPlay muted className="w-100">
+            <source src={videos.AthenaTrailer} type="video/mp4" />
+          </video>
+        )}
       </Card>
     </div>
   );
--- src/component/CustomCarousel/CustomCarousel.js
+++ src/component/CustomCarousel/CustomCarousel.js
@@ -1,6 +1,8 @@
 import Carousel from "react-bootstrap/Carousel";
 import CustomCard from "../CustomCard/CustomCard";
 import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function CustomCarousel(props) {
   const carouselImages = props.carouselImages;
@@ -11,13 +13,17 @@ function CustomCarousel(props) {
         <p>{props.type}</p>
       </Container>
 
-      <Carousel variant="light" pause="hover">
+      <Carousel variant="light" pause="hover" indicators={false}>
         {carouselImages.map((carouselImage, index) => (
           <Carousel.Item key={index}>
             <Container fluid="md" className="d-flex">
-              {carouselImage.map((image, index) => (
-                <CustomCard img={image} key={index} />
-              ))}
+              <Row>
+                {carouselImage.map((image, index) => (
+                  <Col key={index}>
+                    <CustomCard img={image} />
+                  </Col>
+                ))}
+              </Row>
             </Container>
           </Carousel.Item>
         ))}
